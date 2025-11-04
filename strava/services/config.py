6ae289059a0b1390781ff40f260dcaf6c6af6052
Derrick Speirs <141641282+Deslar457@users.noreply.github.com>
2025-11-04 09:04:25 +0000

    Update config.py

--- strava/services/config.py
+++ strava/services/config.py
@@ -1,5 +1,5 @@
 
-client_id = "132964"  # Replace with your actual client_id
-client_secret = "edd5dac7aa72b54840e924a0d558961508cb631a"  # Replace with your actual client_secret
-refresh_token = "9777a200f3d024ff4026f54b3e4d10f69f99ba2c"  # Replace with your actual refresh_token
+client_id = "132964"  # Replace 
+client_secret = "edd5dac7aa72b54840e924a0d558961508cb631a"  # Replace 
+refresh_token = "9777a200f3d024ff4026f54b3e4d10f69f99ba2c"  # Replace 
 
